97c7aaa8bba8dc7f75a377cbe0e119f7acb0ea16
Floris <floris.defeyter@kuleuven.be>
2022-03-18 10:36:43 +0100

    Use tensorboard logger in engine

--- 3_detection/lib/detection/engine.py
+++ 3_detection/lib/detection/engine.py
@@ -3,22 +3,20 @@ import sys
 import time
 import torch
 
+
 import torchvision.models.detection.mask_rcnn
+from tqdm.notebook import tqdm
+
 
 from .coco_utils import get_coco_api_from_dataset
 from .coco_eval import CocoEvaluator
-from .utils import (
-    MetricLogger,
-    SmoothedValue,
-    reduce_dict
-)
+from .utils import reduce_dict
 
 
-def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
+def train_one_epoch(model, optimizer, data_loader, device, epoch,
+                    writer=None):
     model.train()
-    metric_logger = MetricLogger(delimiter="  ")
-    metric_logger.add_meter('lr', SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}]'.format(epoch)
+
 
     lr_scheduler = None
     if epoch == 0:
@@ -28,7 +26,10 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=warmup_factor,
                                                          total_iters=warmup_iters)
 
-    for images, targets in metric_logger.log_every(data_loader, print_freq, header):
+    for batch_idx, (images, targets) in tqdm(enumerate(data_loader),
+                                             total=len(data_loader),
+                                             leave=False,
+                                             desc='Train batch'):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
@@ -54,10 +55,10 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         if lr_scheduler is not None:
             lr_scheduler.step()
 
-        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
-        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-
-    return metric_logger
+        if writer is not None:
+            log_idx = epoch * len(data_loader) + batch_idx
+            writer.add_scalar("Loss/train", losses_reduced, log_idx)
+            writer.add_scalar("LR", optimizer.param_groups[0]["lr"], log_idx)
 
 
 def _get_iou_types(model):
@@ -73,20 +74,19 @@ def _get_iou_types(model):
 
 
 @torch.no_grad()
-def evaluate(model, data_loader, device):
+def evaluate(model, data_loader, device, epoch, writer):
     n_threads = torch.get_num_threads()
     # FIXME remove this and make paste_masks_in_image run on the GPU
     torch.set_num_threads(1)
     cpu_device = torch.device("cpu")
     model.eval()
-    metric_logger = MetricLogger(delimiter="  ")
-    header = 'Test:'
 
     coco = get_coco_api_from_dataset(data_loader.dataset)
     iou_types = _get_iou_types(model)
     coco_evaluator = CocoEvaluator(coco, iou_types)
-
-    for images, targets in metric_logger.log_every(data_loader, 100, header):
+    for batch_idx, (images, targets) in tqdm(enumerate(data_loader),
+                                             total=len(data_loader),
+                                             leave=False, desc='Eval batch'):
         images = list(img.to(device) for img in images)
 
         if torch.cuda.is_available():
@@ -101,11 +101,7 @@ def evaluate(model, data_loader, device):
         evaluator_time = time.time()
         coco_evaluator.update(res)
         evaluator_time = time.time() - evaluator_time
-        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
 
-    # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    print("Averaged stats:", metric_logger)
     coco_evaluator.synchronize_between_processes()
 
     # accumulate predictions from all images
